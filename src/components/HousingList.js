2d4e3cdd0e9b5f11b693e0668df5c4257f701901
littlekanine <elia.kopff3891@gmail.com>
2024-03-08 18:30:15 +0100

    home page update / need Roboto ans it's ok

--- src/components/HousingList.js
+++ src/components/HousingList.js
@@ -1,17 +1,23 @@
-import {housing} from '../datas/housing';
+import Housings from '../datas/housing.json';
 import React from "react";
+import '../styles/index.css'
 import '../styles/housingList.css'
-
+import '../styles/index.css'
 
 function HousingList () {
     return (
-        <div className="container-housing">
-            <ul>
-                {housing.map((house) => (
-                    <li>{house}</li>
-                ))}
-            </ul>
-
+        <div className='background-container flex'>
+            <div className='flex container-housings center'>
+                {
+                Housings.map (housing => {
+                    return (
+                        <div className='flex card align-bottom '>
+                            {housing.title}
+                        </div>
+                    )
+                })
+                }
+            </div>
         </div>
     )
 }
