869e1dc9a6cf8334807348b8f866bdd65cdba7a2
littlekanine <elia.kopff3891@gmail.com>
2024-03-25 11:30:53 +0100

    update animation collapse

--- src/components/collapse/Collapse.js
+++ src/components/collapse/Collapse.js
@@ -1,30 +1,30 @@
-// React component for creating collapsible sections.
-// Accepts two props: 'title' for the title of the collapsible section and 'children' for the content inside the collapsible section.
-
-import React, { useState} from "react";
-import vector from "../../assets/vector/arrow_back_ios-24px 2.png"
-import "./collapse.css"
+import React, { useState } from "react";
+import vector from "../../assets/vector/arrow_back_ios-24px 2.png";
+import "./collapse.css";
 
 function Collapse({ title, children }) {
     const [isOpen, setIsOpen] = useState(false);
-    const [isRotated, setIsRotated] = useState(false);
+    const [initialAnimationComplete, setInitialAnimationComplete] = useState(false);
 
     const toggleCollapse = () => {
         setIsOpen(!isOpen);
-        setIsRotated(!isRotated); // Inverser l'état de rotation à chaque clic
+    };
+
+    const handleAnimationEnd = () => {
+        setInitialAnimationComplete(true);
     };
 
     return (
         <div className="collapse flex center column align-center">
             <div className={`flex collapse-container collapse-header space-between align-center ${isOpen ? 'open' : 'closed'}`}>
                 {title}
-                <img className={`vector flex ${isRotated ? 'rotate-center' : 'rotate-back'}`} src={vector} alt="vector" onClick={toggleCollapse} />
+                <img className={`vector flex ${isOpen ? 'rotate-center' : 'rotate-back'}`} src={vector} alt="vector" onClick={toggleCollapse} />
             </div>
-            <div className={`collapse-content flex ${isOpen ? 'slidein' : 'slideout'}`}>
+            <div className={`collapse-content flex ${initialAnimationComplete ? '' : 'hidden'} ${isOpen ? 'slidein' : 'slideout'}`} onAnimationEnd={handleAnimationEnd}>
                 {children} 
             </div>
         </div>
     );
 }
 
-export default Collapse
+export default Collapse;
